4796858f5e95eedb1b626ac542da379d87d579e5
Elanganathapillai Sivakanthan <siva@globalcitymedia.com>
2020-03-04 16:05:56 +0000

    ArticleDetailView added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# django-blog" 
+"manage.py runserver 127.0.0.1:8081"
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles/detail.html'
